8717ce1c34162ace0ee32a34b4752e49d2afe958
Dwi Sasongko S <ruckuus@gmail.com>
2014-12-06 11:13:23 +0700

    Init

---
+++ features/fans.feature
@@ -0,0 +1,11 @@
+Feature:
+  Fans news client
+
+  As a footbal enthusiast
+  I want to be updated with football news
+
+  Scenario:
+    Given I launch the app
+    Then I should see the list of football news
+    
+
---
+++ features/step_definition/fans_test.rb
@@ -0,0 +1,7 @@
+Given(/^I launch the app$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the list of football news$/) do
+    pending # express the regexp above with the code you wish you had
+end
